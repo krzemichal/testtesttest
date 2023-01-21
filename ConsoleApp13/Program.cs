cc5006420a690fc6f00ea40190df0420a69f953e
ultron682 <52131708+ultron682@users.noreply.github.com>
2023-01-21 12:28:54 +0100

    Add project files.

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        void adsdasd()
+        {
+
+
+        }
+    }
+}
